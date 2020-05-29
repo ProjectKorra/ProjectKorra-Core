3333e3c100c68fcd20bd1f6d9e9f5da5915ec69d
Christopher Martin <Christopherwmm@gmail.com>
2020-05-29 00:23:32 -0700

    Initial project upload (#1)
    Uploads the initial project structure, some of the related GitHub repository files, and the Gradle build script.

---
+++ src/main/java/com/projectkorra/core/ProjectKorra.java
@@ -0,0 +1,12 @@
+package com.projectkorra.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ProjectKorra extends JavaPlugin {
+
+	@Override
+	public void onEnable() {}
+
+	@Override
+	public void onDisable() {}
+}
