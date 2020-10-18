3219cd7c74ccb7187929121722e4993e44bb33f8
Simp <simplicitee123@gmail.com>
2020-10-18 12:53:48 -0400

    updates

--- src/main/java/com/projectkorra/core/system/skill/Skill.java
+++ src/main/java/com/projectkorra/core/system/skill/Skill.java
@@ -1,11 +1,21 @@
 package com.projectkorra.core.system.skill;
 
-import net.md_5.bungee.api.ChatColor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.commons.lang.Validate;
+
+import com.google.common.collect.ImmutableSet;
+import com.projectkorra.core.util.text.DisplayVariant;
 
 public class Skill {
+	
+	private static final Map<String, Skill> SKILLS = new HashMap<>();
 
-	private String name, verb, user, description;
-	private ChatColor color;
+	private DisplayVariant display;
+	private String description;
+	private ImmutableSet<Skill> parents, children;
 	
 	/**
 	 * A skill is something that entities can do, common examples of this
@@ -17,35 +27,65 @@ public class Skill {
 	 * @param description explanation of the skill
 	 * @param color text color associated with this skill
 	 */
-	Skill(String name, String verb, String user, String description, ChatColor color) {
-		this.name = name;
-		this.verb = verb;
-		this.user = user;
+	Skill(DisplayVariant display, String description) {
+		this.display = display;
 		this.description = description;
-		this.color = color;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String getVerb() {
-		return verb;
-	}
-
-	public String getUser() {
-		return user;
+	/**
+	 * Returns the internal name of this skill, which is lowercased
+	 * @return internal skill name
+	 */
+	public String getInternalName() {
+		return display.getNoun().toLowerCase();
 	}
 
 	public String getDescription() {
 		return description;
 	}
-
-	public ChatColor getColor() {
-		return color;
+	
+	public DisplayVariant getDisplay() {
+		return display;
 	}
 	
-	public String getDisplay() {
-		return color + name;
+	public ImmutableSet<Skill> getChildren() {
+		return children;
+	}
+	
+	public ImmutableSet<Skill> getParents() {
+		return parents;
+	}
+	
+	public static Optional<Skill> getByName(String name) {
+		return Optional.ofNullable(SKILLS.get(name));
+	}
+	
+	/**
+	 * Attempt to register a new skill with the given parameters
+	 * @param display how the skill will be displayed
+	 * @param description description of the skill
+	 * @param parents which skills this one derives from
+	 * @return newly registered skill
+	 */
+	public static Skill register(DisplayVariant display, String description, Skill... parents) {
+		Validate.isTrue(display != null, "Attempted to register Skill with null display");
+		Validate.isTrue(display.isValid(), "Attempted to register Skill with invalid display (data inside the display is null)");
+		Validate.isTrue(!SKILLS.containsKey(display.getNoun().toLowerCase()), "Attempted to register Skill with pre-existing name");
+		
+		Skill skill = new Skill(display, description);
+		SKILLS.put(display.getNoun().toLowerCase(), skill);
+		
+		for (Skill parent : parents) {
+			if (parent == null) {
+				continue;
+			}
+			
+			parent.children = new ImmutableSet.Builder<Skill>().addAll(parent.children).add(skill).build();
+		}
+		
+		skill.parents = ImmutableSet.copyOf(parents);
+		skill.children = ImmutableSet.of();
+		
+		return skill;
 	}
 }
---
+++ src/main/java/com/projectkorra/core/util/text/DisplayVariant.java
@@ -0,0 +1,48 @@
+package com.projectkorra.core.util.text;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class DisplayVariant {
+
+	private String noun, verb, user;
+	private ChatColor color;
+	
+	public DisplayVariant(String noun, String verb, String user, ChatColor color) {
+		this.noun = noun;
+		this.verb = verb;
+		this.user = user;
+		this.color = color;
+	}
+
+	public String getNoun() {
+		return noun;
+	}
+
+	public String getVerb() {
+		return verb;
+	}
+
+	public String getUser() {
+		return user;
+	}
+	
+	public ChatColor getColor() {
+		return color;
+	}
+	
+	public String getColoredNoun() {
+		return color + noun;
+	}
+	
+	public String getColoredVerb() {
+		return color + verb;
+	}
+	
+	public String getColoredUser() {
+		return color + user;
+	}
+	
+	public boolean isValid() {
+		return noun != null && verb != null && user != null && color != null;
+	}
+}
