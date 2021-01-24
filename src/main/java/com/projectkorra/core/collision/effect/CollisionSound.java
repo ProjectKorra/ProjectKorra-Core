d978a7cd25238e1b2d3c2e79f32eb270e4caacf4
Simp <simplicitee123@gmail.com>
2021-01-23 19:52:24 -0500

    Create CollisionSound.java

---
+++ src/main/java/com/projectkorra/core/collision/effect/CollisionSound.java
@@ -0,0 +1,29 @@
+package com.projectkorra.core.collision.effect;
+
+import org.bukkit.Location;
+import org.bukkit.Sound;
+
+import com.projectkorra.core.event.BendingCollisionEvent;
+import com.projectkorra.core.util.data.Pairing;
+
+public class CollisionSound extends CollisionEffect {
+
+	public CollisionSound() {
+		super("sound", Pairing.of(STRING, Sound.ENTITY_GENERIC_EXPLODE.name()), Pairing.of(FLOAT, 0.8f), Pairing.of(FLOAT, 0.2f));
+	}
+
+	@Override
+	protected void accept(BendingCollisionEvent event, Object... args) {
+		Sound sound;
+		
+		try {
+			sound = Sound.valueOf((String)args[0]);
+		} catch (Exception e) {
+			sound = Sound.ENTITY_GENERIC_EXPLODE;
+		}
+		
+		Location loc = event.getCenterOfMass();
+		loc.getWorld().playSound(loc, sound, (float)args[1], (float)args[2]);
+	}
+
+}
