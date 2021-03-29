28fda95802a7de5e96a1b9531848dcfda60539a6
Simplicitee <simplicitee123@gmail.com>
2021-03-29 18:05:37 -0400

    tidy up combos

--- src/main/java/com/projectkorra/core/system/ability/type/Combo.java
+++ src/main/java/com/projectkorra/core/system/ability/type/Combo.java
@@ -2,11 +2,9 @@ package com.projectkorra.core.system.ability.type;
 
 import java.util.List;
 
-import com.projectkorra.core.system.ability.Ability;
-import com.projectkorra.core.system.ability.activation.Activation;
-import com.projectkorra.core.util.data.DistinctPair;
+import com.projectkorra.core.system.ability.activation.SequenceInfo;
 
 public interface Combo {
 
-	public List<DistinctPair<Ability, Activation>> getSequence();
+	public List<SequenceInfo> getSequence();
 }
